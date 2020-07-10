d87790cc93894fb892e66ea832f18faf38267c87
PapiCarryShongo <jeca010198@gmail.com>
2020-07-10 10:39:30 -0500

    Servidor React Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <Router>
+      <h1>Servidor React front</h1>
+    </Router>
+  );
+}
+
+export default App;
